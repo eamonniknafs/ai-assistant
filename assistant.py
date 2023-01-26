1bec188d76eba5f1b9b59fd3e91ef921cb3a7e83
eamonniknafs <en@bu.edu>
2023-01-25 21:49:04 -0500

    increased max tokens a lot

--- assistant.py
+++ assistant.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def run():
     query = listen()
-    response = openai.Completion.create(model="text-davinci-003", prompt="You are a very intelligent voice assistant. Respond to this query: " + query, temperature=0, max_tokens=20)
+    response = openai.Completion.create(model="text-davinci-003", prompt="You are a very intelligent voice assistant. Respond to this query: " + query, temperature=0, max_tokens=3000)
     speak(response.choices[0].text)
 
 if __name__ == "__main__":
