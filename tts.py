32a995d6fbeebf2241822385227379812b5fccb7
eamonniknafs <en@bu.edu>
2023-01-25 22:01:10 -0500

    added support for non-macOS machines

--- tts.py
+++ tts.py
@@ -1,5 +1,11 @@
 import subprocess
+import platform
+import pyttsx3
 
 def speak(text):
-    subprocess.call(['say', text])
-    
+    if platform.system() == "Darwin":
+        subprocess.call(['say', text])
+    else:
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
