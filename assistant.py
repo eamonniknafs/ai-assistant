9291441b47f09e9ddeaeab015552f76ab4f81fa2
eamonniknafs <en@bu.edu>
2023-01-25 22:10:48 -0500

    updated prints

--- assistant.py
+++ assistant.py
@@ -10,13 +10,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def run():
     query = listen()
     response = openai.Completion.create(model="text-davinci-003", prompt="You are a very intelligent voice assistant. Respond to this query: " + query, temperature=0, max_tokens=3000)
+    print(response.choices[0].text)
     speak(response.choices[0].text)
 
 if __name__ == "__main__":
     while True:
         try:
             # Wait for sparcebar to be pressed
-            print("Press return to start the assistant")
+            print("\n---------------\nPress return to start the assistant")
             input()
 
             # Run the assistant
