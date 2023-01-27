07e5f49a01932ea440461e932d0d893d1fb092ed
eamonniknafs <en@bu.edu>
2023-01-27 16:49:37 -0500

    reduced max tokens

--- assistant.py
+++ assistant.py
@@ -10,7 +10,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def run():
     print("Listening...")
     query = listen()
-    response = openai.Completion.create(model="text-davinci-003", prompt="You are a general intelligence AI. Respond to this query: " + query, temperature=0, max_tokens=3000)
+    response = openai.Completion.create(model="text-davinci-003", prompt="You are a general intelligence AI. Respond to this query: " + query, temperature=0, max_tokens=2000)
     print(response.choices[0].text)
     speak(response.choices[0].text)
 
